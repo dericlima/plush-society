5dd1ea7eb3ff139886a028d2f0ec54d191847108
dericlima <dericlima@gmail.com>
2021-11-01 19:21:06 +0100

    Remove UI components

--- js/packages/webvue/src/App.vue
+++ js/packages/webvue/src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <wallet-provider :wallets="wallets" auto-connect>
-    <div>
-      <wallet-modal-provider>
-        <wallet-multi-button></wallet-multi-button>
-      </wallet-modal-provider>
-    </div>
-    <img alt="Vue logo" src="./assets/plush1.jpg">
-    <HelloWorld msg="Welcome to the Plush Society"/>
-  </wallet-provider>
+    <button>Connect</button>
+    <img alt="Plush Society logo" src="./assets/plush1.jpg">
+    <HelloWorld msg="Welcome to the Plush Society" />
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import {getPhantomWallet} from '@solana/wallet-adapter-wallets'
+import {initWallet} from '@solana/wallet-adapter-vue'
 export default {
   name: 'App',
   components: {
@@ -22,6 +17,10 @@ export default {
     const wallets = [
       getPhantomWallet(),
     ]
+
+    initWallet({
+      wallets: [wallets], autoConnect: true
+    })
     return {
       wallets
     }
